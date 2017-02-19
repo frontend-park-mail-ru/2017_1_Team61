075b10a7560d63c2819e051bf7dccf0ef878ccd8
XfortunaX <buklin.sergei@yandex.ru>
2017-02-19 23:38:08 +0300

    load_new

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/static'));
+
+app.listen(3000, function () {
+    console.log('Listen port: 3000!');
+});
