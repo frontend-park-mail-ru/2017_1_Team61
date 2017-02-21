9a7031839d4fac78e9d3eba86c0a80eda638094d
XfortunaX <buklin.sergei@yandex.ru>
2017-02-21 23:22:26 +0300

    login&&sign_up2.0

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use(express.static(__dirname + '/static'));
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log('Listen port: 3000!');
 });
