ecf464d5d6ea189d01acee965dedd6bec545128e
XfortunaX <buklin.sergei@yandex.ru>
2017-02-21 23:17:24 +0300

    login&&sign_up

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sample
+
+Для запуска:
+
+1. `npm start`
+2. `http://localhost:3000/`
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express();
 
 app.use(express.static(__dirname + '/static'));
 
+const port = process.env.PORT || 3000;
+
 app.listen(3000, function () {
     console.log('Listen port: 3000!');
 });
