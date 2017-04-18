9476e409a2aacc538b0f2dbeda7ed02f89157ffd
Tlakatlekutl <ary3004@yandex.ru>
2017-04-19 00:33:00 +0300

    Перенес конфигвебпака  в корень

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './main.js',
+  entry: './static/js/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'static/dist'),
   },
   watch: true,
   node: {
