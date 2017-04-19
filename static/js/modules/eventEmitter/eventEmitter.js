250b68303ac5549ab94e2428caace82eabca4b89
Tlakatlekutl <ary3004@yandex.ru>
2017-04-19 22:34:24 +0300

    Добавил playground для wevsocket

--- static/js/modules/eventEmitter/eventEmitter.js
+++ static/js/modules/eventEmitter/eventEmitter.js
@@ -7,10 +7,21 @@ export default class EventEmitter {
     if (EventEmitter.instance) {
       return EventEmitter.instance;
     }
+    this.events = [];
 
     EventEmitter.instance = this;
   }
   on(event, listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError('listener is not a function');
+    }
+    this.events.push({event, listener});
+
+  }
+  emit(name, payload = null) {
+
+  }
+  off() {
 
   }
 }
--- static/js/views/mpGameView.js
+++ static/js/views/mpGameView.js
@@ -4,16 +4,38 @@
 import BaseView from './baseView';
 import template from '../templates/mp.pug';
 
+const ws = new WebSocket('ws://62.109.3.208:8082/game');
+ws.onopen = () => {
+  console.log('Соединение установлено');
+  ws.send(JSON.stringify({
+    type: 'com.aerohockey.mechanics.requests.JoinGame$Request',
+    content: '{}',
+  }));
+};
+ws.onclose = (event) => { console.log(`socket close ${event.code}`); };
+
+
 export default class MpGameView extends BaseView {
   constructor() {
     super(['multiplayer-game-view'], template);
     this.render();
     this.addEventListeners();
+    this.addWebsocket();
   }
   addEventListeners() {
-    const x = document.querySelector('.result');
-    document.querySelector('.goleft').addEventListener('click', () => { x.innerHTML = 'left'; });
-    document.querySelector('.goright').addEventListener('click', () => { x.innerHTML = 'right'; });
+    this.x = document.querySelector('.result');
+    document.querySelector('.goleft').addEventListener('click', () => {
+      ws.send('left');
+    });
+    document.querySelector('.goright').addEventListener('click', () => {
+      ws.send('right');
+    });
+  }
+  addWebsocket() {
+    ws.onmessage = (event) => {
+      console.log(event.data);
+      this.x.innerHTML = event.data;
+    };
   }
 }
 
