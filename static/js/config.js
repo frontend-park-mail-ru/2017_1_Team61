3ad70ab2cdaa94b5f1e86a890bd7632d6cbf6d03
Tlakatlekutl <ary3004@yandex.ru>
2017-05-27 01:11:57 +0300

    nginx add proxy 5

--- static/js/config.js
+++ static/js/config.js
@@ -3,6 +3,8 @@
  */
 
 export const API_URL = 'https://fastball-dev.herokuapp.com/api';
-export const WS_URL = 'ws://62.109.3.208:8082/game';
+export const WS_URL = 'wss://fastball-dev.herokuapp.com/game';
+
+// export const WS_URL = 'wss://62.109.3.208:8082/game';
 
 // https://fastball-backend.herokuapp.com/api', /// 'http://62.109.3.208:8082/api'
