00c7b8f6f57cc42ce60d63cd515cc27080923fdc
Tlakatlekutl <ary3004@yandex.ru>
2017-06-02 17:03:49 +0300

    change urls

--- static/js/config.js
+++ static/js/config.js
@@ -5,8 +5,11 @@
 // export const API_URL = 'https://fastball-dev.herokuapp.com/api';
 // export const WS_URL = 'wss://fastball-dev.herokuapp.com/ws';
 
-export const API_URL = 'http://185.158.154.191:8082/api';
-export const WS_URL = 'ws://185.158.154.191:8082/ws';
+// export const API_URL = 'http://185.158.154.191:8082/api';
+// export const WS_URL = 'ws://185.158.154.191:8082/ws';
+
+export const API_URL = 'http://62.109.3.208:8082/api';
+export const WS_URL = 'ws://62.109.3.208:8082/ws';
 
 // export const WS_URL = 'wss://62.109.3.208:8082/game';
 
